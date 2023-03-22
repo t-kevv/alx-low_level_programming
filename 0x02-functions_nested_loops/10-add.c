2623389af6935e973697b4f13754957ab00392c8
kevin <kevintambe20@gmail.com>
2023-03-22 09:14:32 +0000

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two values
+ * @n1:first number to be computer
+ * @n2: second number to be computer
+ * Return: Always 0 (Success)
+*/
+
+int add(int n1, int n2)
+
+{
+	return (n1 + n2);
+}
