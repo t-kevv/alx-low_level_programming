108d1f8e4143fdd7353f7276ce0fcd192bf75992
kevin <kevintambe20@gmail.com>
2023-04-03 22:18:25 +0000

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums  - entry point
+ * @a: number
+ * @size: number
+ *
+ * Return: always 0
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, n, num1 = 0, num2 = 0;
+
+	for (i = 0; i <= (size * size); i = i + size + 1)
+	{
+		num1 = num1 + a[i];
+	}
+
+	for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
+	{
+		num2 = num2 + a[n];
+		printf("%d, %d\n", num1, num2);
+	}
+}
