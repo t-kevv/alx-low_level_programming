491152702bf19fb970ac3d892a2a16908709ccf4
kevin <kevintambe20@gmail.com>
2023-04-03 21:17:00 +0000

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * _strpbrk - entry point
+ * @s: number
+ * @accept: number
+ *
+ * Return: always 0
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, n;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	for (n = 0; accept[n] != '\0'; n++)
+	{
+	if (s[i] == accept[n])
+	{
+		return (s + i);
+	}
+	}
+	}
+
+	return (null);
+}
