1c17f2acde496b51f56d3c0f8eddb9b53fbc3027
kevin <kevintambe20@gmail.com>
2023-04-03 22:24:44 +0000

    8-print_diagsums.c updated

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -14,9 +14,7 @@ void print_diagsums(int *a, int size)
 	int i, n, num1 = 0, num2 = 0;
 
 	for (i = 0; i <= (size * size); i = i + size + 1)
-	{
 		num1 = num1 + a[i];
-	}
 
 	for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
 	{
