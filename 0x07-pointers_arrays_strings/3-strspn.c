cebb61eba0ac1e28a5d9ff043b24b0cab940955a
kevin <kevintambe20@gmail.com>
2023-04-03 21:04:19 +0000

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strspn - entry point
+ * @s: pointer
+ * @accept: pionter
+ *
+ * Return: always 0
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int j, p, m, c;
+
+	value = 0;
+
+	for (j = 0; s[j] != '\0'; j++)
+	{
+	c = 0;
+
+	for (p = 0; accept[p] != '\0'; p++)
+	{
+	if (accept[p] == s[j])
+	{
+		m++;
+		c = 1;
+	}
+	}
+
+	}
+}
