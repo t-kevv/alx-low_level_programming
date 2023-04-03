7149f063d5bbd5e116934285e1059c90b7430c0c
kevin <kevintambe20@gmail.com>
2023-04-03 21:50:23 +0000

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - entry point
+ * @haystack: number
+ * @needle: number
+ *
+ * Return: always 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+	char *num1 = haystack;
+	char *num2 = needle;
+
+	while (*num1 == *num2 && *num2 != '\0')
+	{
+	num1++;
+	num2++;
+	}
+
+	if (+num2 == '\0')
+	{
+	return (haystack);
+	}
+	}
+
+	return (NULL);
+}
