6e9624d8ebff07515db33387487019dc85ac6ab0
kevin <kevintambe20@gmail.com>
2023-04-10 01:49:26 +0000

    first commit

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - uppercase letters
+ * @c: to  check
+ *
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'a' && c <= 'a')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
+
