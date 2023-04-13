eb19588d95ce0e8fe19a97ce3add41891d6ce728
kevin <kevintambe20@gmail.com>
2023-04-13 18:47:46 +0000

    100.realloc.c updated

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,7 +15,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i;
 
 	if (new_size == old_size)
+	{
 		return (ptr);
+	}
 
 	if (ptr == NULL)
 	{
@@ -26,6 +28,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 		return (nptr);
 	}
+
 	else
 	{
 		if (new_size == 0)
@@ -38,7 +41,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	nptr = malloc(new_size);
 
 	if (nptr == NULL)
+	{
 		return (NULL);
+	}
 
 	for (i = 0; i < old_size && i < new_size; i++)
 	{
