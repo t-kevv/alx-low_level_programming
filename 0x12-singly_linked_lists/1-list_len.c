53ade8657d7cf146b068c92971ae270657f10242
kevin <kevintambe20@gmail.com>
2023-04-27 21:47:00 +0000

    first commit

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements
+ * @h: pointer
+ * Return: number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
+
