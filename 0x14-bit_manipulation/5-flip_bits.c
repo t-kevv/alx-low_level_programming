e27943561f62ac041cc06ce17158ceac560878ae
kevin <kevintambe20@gmail.com>
2023-05-04 16:48:03 +0000

    5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * flip_bits - counts number of bit
+ * @n: number
+ * @m: number
+ *
+ * Return: number of bits
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int f;
+	int ct = 0;
+	unsigned long int cr;
+	unsigned long int dm = n ^ m;
+
+	for (f = 63; f >= 0; f--)
+	{
+	cr = dm >> f;
+	if (cr & 1)
+	ct++;
+	}
+
+	return (ct);
+}
+
