97fb577b2ca0c38d3b76e52e2ab69d853f31df3a
kevin <kevintambe20@gmail.com>
2023-05-04 16:27:23 +0000

    0-binary_to_uint.c updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -19,7 +19,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[f] < '0' || b[f] > '1')
 			return (0);
-		res = 2 * res + (b[i] - '0');
+		res = 2 * res + (b[f] - '0');
 	}
 
 	return (res);
