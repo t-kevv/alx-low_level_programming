f06f432dd48f0514cd12edc664fd74fd91cffde0
kevin <kevintambe20@gmail.com>
2023-05-08 17:17:21 +0000

    1-create_file.c updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,33 +10,28 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int h, i, l = 0;
+	int fd, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content != NULL)
-	{
-		while (text_content[l])
-			l++;
-	}
-
-	h = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (h == -1)
-		return (-1);
 
 	if (text_content != NULL)
 	{
-		i = write(h, text_content, l);
-		if (i == -1)
-		{
-			close(h);
-			return (-1);
-		}
+		for (len = 0; text_content[len];)
+			len++;
 	}
-	if (close(h) == -1)
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
 		return (-1);
+
+	close(fd);
+
 	return (1);
 }
+
 
 
 
