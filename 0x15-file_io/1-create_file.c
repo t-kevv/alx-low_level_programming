f1a483a3eab4bd5a941e31fd6b56c46ba721d39a
kevin <kevintambe20@gmail.com>
2023-05-08 17:08:58 +0000

    1-create_file.c updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	h = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(h, text_content, j);
+	i = write(h, text_content, j);
 
 	if (h == -1 || i == -1)
 	return (-1);
